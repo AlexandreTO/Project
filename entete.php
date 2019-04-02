f73cb2477542269181f926e201888e3b69c0dc0b
Alexandre To <alexandreto@gmail.com>
2019-04-02 22:31:46 +0200

    test

--- entete.php
+++ entete.php
@@ -1,7 +1,7 @@
 <nav class="navbar  fixed-top bg-light">
         <a class="nav-link active" href="#">Accueil</a>
         <a class="nav-link" href="#">Catalogue</a>
-        <!-- <img src="images/logo.png" alt="logo" class ="logo" width="20" height="20"> -->
+        <img src="images/logo.png" alt="logo" class ="logo" width="20" height="20">
         <a class="nav-link" href="#">Services</a>
         <a class="nav-link" href="#">Contact</a>
     </nav>
