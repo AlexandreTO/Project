2c17beff561aab9b294c5d98b827f50f64a20755
Alexandre To <alexandreto@gmail.com>
2019-04-08 11:04:03 +0200

    Ajout de la page catalogue (vitrine)

---
+++ Catalogue.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Catalogue</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="all" href="main.css">
+    <link rel="stylesheet" href="catalogue.css">
+    <script src="main.js"></script>
+</head>
+
+<body>
+    <header class="navbar ">
+        <?php include("entete.php");?>
+    </header>
+
+    <main>
+        <h1>Catalogue</h1>
+        <br/>
+        <div class="ligne">
+            <div class="colonne">
+                <img src="images/image.png">
+                <img src="images/logo.png">
+            </div>
+            <div class="colonne">
+                <img src="images/brian-chan-12168-unsplash.jpg">
+            </div>
+            <div class="colonne">
+                <img src="images/jordane-mathieu-539949-unsplash.jpg" >
+            </div>
+        </div>
+        </section>
+    </main>
+</body>
+</html>
--- entete.php
+++ entete.php
@@ -1,8 +1,9 @@
 <nav class="navbar  fixed-top bg-light">
         <!-- alignement centre -->
-        <a class="nav-link active" href="#">Accueil</a>
-        <a class="nav-link" href="#">Catalogue</a>
+        <a class="nav-link" href="site.php">Accueil</a>
+        <a class="nav-link" href="Catalogue.php">Catalogue</a>
         <!-- <img src="images/logo.png" alt="logo" class ="logo" width="20" height="20"> -->
         <a class="nav-link" href="#">Services</a>
         <a class="nav-link" href="#">Contact</a>
     </nav>
+    <br/>
