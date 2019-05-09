4dd1235fe11e4110b897929cfff24579a4fc8e02
Christopher William <christopherwilliam@macbook-pro-de-christopher.home>
2019-05-09 15:12:16 +0200

    ajout page connexion

---
+++ connexion.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Accueil</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" media="screen" href="Bootstrap/css/bootstrap.css" />
+    <link href="Bootstrap/css/mdb.css" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+
+    <script src="Bootstrap/js/jquery-3.3.1.min.js"></script>
+    <script src="Bootstrap/js/popper.min.js"></script>
+    <script src="Bootstrap/js/bootstrap.min.js"></script>
+    <script src="Bootstrap/js/mdb.min.js"></script>
+    <script src="main.js"></script>
+    <!-- DANS CET ORDRE -->
+</head>
+
+<body>
+    <header>
+        <?php include("entete.php"); ?>
+    </header>
+        <!-- Formulaires de contact -->
+        <main>
+
+            <h2>Connexion</h2>
+
+            <form action="" class="login">
+                <input type="text" name="Identifiant" id="identifiant" placeholder="Identifiant"><br><br>
+                
+                <input type="password" name="password" id="password" placeholder="mot de passe"><br><br>
+
+                <input type="submit" id="connexion" value="Connexion"><br><br>
+
+
+
+
+
+
+                
+
+                
+
+            </form>
+        </main>
+</body>
+
+</html>
