2aa71597da34965ba8703aa9c16b69c2a1ca3c59
Alexandre <alexandreto1996@gmail.com>
2019-05-25 12:51:45 +0200

    Mise à jour de la page Services

--- Services.php
+++ Services.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Services</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Connexion</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" media="screen" href="Bootstrap/css/bootstrap.css" />
+    <link href="Bootstrap/css/mdb.css" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+
+    <script src="Bootstrap/js/jquery-3.3.1.min.js"></script>
+    <script src="Bootstrap/js/popper.min.js"></script>
+    <script src="Bootstrap/js/bootstrap.min.js"></script>
+    <script src="Bootstrap/js/mdb.min.js"></script>
+    <script src="main.js"></script>
+    <!-- DANS CET ORDRE -->
 </head>
 
 <body>
-    <header class="navbar ">
-        <?php include("entete.php");?>
-    </header>
+    <nav><?php include("entete.php"); ?></nav>
 </body>
 
 </html>
